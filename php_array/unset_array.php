99ee4db35c6714b724435dd3cd710e8af10f279a
wangjun2012al <wangjun2012al@gmail.com>
2014-10-21 20:17:34 +0800

    11

---
+++ php_array/unset_array.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+	/*
+		array_values() 重组数组使得索引值重新排列
+
+	*/
+	
+	$arr=["one","two","three","fore"];
+
+	print_r($arr);
+	echo "原始数组<br>";
+	unset($arr[2]);
+	
+	print_r($arr);
+	echo "删除后数组<br>";
+	$arr_r=array_values($arr);
+	print_r($arr_r);
+	echo "重组后数组<br>";
+

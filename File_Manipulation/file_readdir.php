1c28b3e23b6ab0ce11104aa0b45c4431703c9970
root <root@localhost.localdomain>
2014-12-05 00:30:10 +0800

    * modify file_readdir.php

--- File_Manipulation/file_readdir.php
+++ File_Manipulation/file_readdir.php
@@ -10,10 +10,16 @@
     }
     
     echo "<hr>";
+    //opendir()&&closedir()&&readdir&&rewinddir()
     $etc=opendir("/etc");
         //一次只读取一个目录
+        //下面的代码会读取出.&..
         echo(readdir($etc));
-
+        echo(readdir($etc));
+    //遍历这个目录
+    while(readdir($etc)){
+    echo(readdir($etc)."<br>");
+    }
 
     closedir($etc);
 ?>      

5077cdbea04dcbf7a1f46723dc41caf1c93d7c02
wangjun2012al <wangjun2012al@gmail.com>
2014-12-05 21:14:21 +0800

    * modified the file file_easyfuncs.php

--- File_Manipulation/file_easyfuncs.php
+++ File_Manipulation/file_easyfuncs.php
@@ -9,16 +9,17 @@
     copy("/home/AlixWang/Desktop/test1.php","/home/AlixWang/Desktop/test2.php");
 
     //rename()重命名文件
-    rename("/home/AlixWang/Desktop/test1.php","/home/AlixWang/Desktop/test3.php");
+    rename("/home/AlixWang/Desktop/test2.php","/home/AlixWang/Desktop/test3.php");
 
     //file_put_contents()向文件中写内容
+
     file_put_contents("/home/AlixWang/Desktop/test1.php","this is a test file");
 
     //file_get_contents()读取文件中的内容
     echo(file_get_contents("/home/AlixWang/Desktop/test1.php")."<br>");
     //fopen()打开文件ftruncate()截取文件的一段
     $fp=fopen("/home/AlixWang/Desktop/test1.php","w");
-    ftruncate($fp,100);
+    ftruncate($fp,10);
     
     //file()
     $arr=file("/home/AlixWang/Desktop/test1.php");

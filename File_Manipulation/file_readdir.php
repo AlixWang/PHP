fe4aa97f89c9cc77f612c4ea9fd3f68ac36d83c1
wangjun2012al <wangjun2012al@gmail.com>
2014-12-05 15:14:59 +0800

    * modified file_readder.php

--- File_Manipulation/file_readdir.php
+++ File_Manipulation/file_readdir.php
@@ -11,15 +11,37 @@
     
     echo "<hr>";
     //opendir()&&closedir()&&readdir&&rewinddir()
-    $etc=opendir("/etc");
+    $etc=opendir("/var/www/html/PHP/GitHub");
         //一次只读取一个目录
         //下面的代码会读取出.&..
-        echo(readdir($etc));
-        echo(readdir($etc));
-    //遍历这个目录
-    while(readdir($etc)){
-    echo(readdir($etc)."<br>");
-    }
 
-    closedir($etc);
+        //echo(readdir($etc));
+        //echo(readdir($etc));
+
+    //遍历这个目录
+    while($dir=readdir($etc)){
+    //不要操作.&..
+    if($dir!="."&&$dir!=".."){
+        //注意此处$dir一定要用当前文件的相对位置表示
+        $dir="../".$dir;
+        if(is_dir($dir)){
+            echo("directory ==> ".$dir."<br>");
+            }
+        else{
+            echo "file ==> ".$dir."<br>";
+        }
+        }
+    }
+    
+    rewinddir($etc);    //指针回到第一个
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+    echo $dir;
+    echo $dir;
+    echo $dir;
+    echo $dir;
+    echo $dir;
+    echo $dir;
+    echo $dir;
+    echo $dir;
+    closedir($etc); //关闭目录
 ?>      

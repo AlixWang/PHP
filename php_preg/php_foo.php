2094f61d92f14844f55f1528e670ee2c12a62a8c
wangjun2012al <wangjun2012al@gmail.com>
2014-12-25 01:18:06 +0800

    * teh eg of preg_match&&preg_replave

--- php_preg/php_foo.php
+++ php_preg/php_foo.php
@@ -4,6 +4,10 @@
     $targ="<table>This is test of how to test preg </table>>";
     if(preg_match('/^<table>/',$targ)){
         echo "IT is ok";
+        echo "<br>";
     }
 
-
+    //preg_replace
+    $targ1='1234213';
+    $targ2=preg_replace('/^\d{3}/','what',$targ1);
+    echo 'the origen char is'.$targ1.'the over is '.$targ2;

8f7086134600a21cb58f5e3b6793de8c83b1b57a
wangjun2012al <wangjun2012al@gmail.com>
2014-12-26 01:49:44 +0800

    * naming for the array

--- php_preg/preg_match.php
+++ php_preg/preg_match.php
@@ -14,3 +14,18 @@
         print_r($matches);
 	}
 
+
+	//命名捕获
+	$url="https://www.baidu.com/index.heml";
+
+	if (preg_match('{^(?P<proto>\w+)://(?P<host>[^:/]+):?(?P<port>\d+)?(?P<dirname>/.*)?}x',$url,$matches)) {
+		$proto=$matches['proto'];
+		$host=$matches['host'];
+		$port=$matches['port']?$matches['port']:($proto=='http'||'https'?80:443);
+
+		echo "Protocol:".$proto;
+		echo "host:".$host;
+		echo "Port:".$port;
+        print_r($matches);
+	}
+

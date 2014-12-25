5e337532d56527982e36fe3890eacb24308e84c5
wangjun2012al <wangjun2012al@gmail.com>
2014-12-26 01:41:43 +0800

    * get it to onederful

--- php_preg/preg_match.php
+++ php_preg/preg_match.php
@@ -1,9 +1,9 @@
 <?php
 
 	//提取url中的协议，主机名，端口号
-	$url="https://www.baidu.com:80/index.heml";
+	$url="https://www.baidu.com/index.heml";
 
-	if (preg_match('{^(\b)://((?:\b\.?)+):?(\d+)(/.*)?$}',$url,$matches)) {
+	if (preg_match('{^(\w+)://([^:/]+):?(\d+)?(/.*)?}x',$url,$matches)) {
 		$proto=$matches[1];
 		$host=$matches[2];
 		$port=$matches[3]?$matches[3]:($proto=='http'||'https'?80:443);
@@ -11,5 +11,6 @@
 		echo "Protocol:".$proto;
 		echo "host:".$host;
 		echo "Port:".$port;
+        print_r($matches);
 	}
 

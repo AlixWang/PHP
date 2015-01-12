1f264f0fd43acad569af06293a56fed96834fea7
wangjun2012al <wangjun2012al@gmail.com>
2015-01-12 17:16:17 +0800

    * chmod 777 to the file

---
+++ php_GD/imagecreate_GD.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ *
+ *
+ * */
+
+ //创建画布
+    $img=imagecreatetruecolor(200,200);
+
+ //设置颜色
+    $white=imagecolorallocate($img,255,255,255);
+    $red=imagecolorallocate($img,255,0,0);
+    $blue=imagecolorallocate($img,0,0,255);
+
+ //填充画布
+    imagefill($img,0,0,$red);
+       
+ //保存图象
+    //imagepng($img,'./test.png');
+    //imagegif($img,'./test,gif');
+    //imagejpeg($img,'./test.jpg');
+ 
+ //直接输出
+    header("Content-Type:image/png");
+    imagepng($img);
+ //释放资源   
+    imagedestroy($img);    
+?>
